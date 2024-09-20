701c7ef856fbaf1137a4126d93371c9ec58ad94e
Robin Pahlman <robin.pahlman@gmail.com>
2024-09-20 10:15:47 -0400

    update phone #

--- src/app/text-us.jsx
+++ src/app/text-us.jsx
@@ -3,8 +3,13 @@ export default function TextUs() {
     <div className="container py-8 px-4 text-center">
       <h2 className="text-3xl font-bold mb-8">Questions? Text us!</h2>
       <p className="text-2xl text-secondary mb-4">(347) 635-4588</p>
-      <a className="btn btn-primary animate-pulse" href="sms://+123456789&?body=Hi, I'm interested in guitar lessons.">Text us!</a>
+      <a
+        className="btn btn-primary animate-pulse"
+        href="sms://+13476354588&?body=Hi, I'm interested in guitar lessons."
+      >
+        Text us!
+      </a>
       <div className="text-5xl animate-bounce pl-16 mt-2">👆</div>
     </div>
-  )
+  );
 }
