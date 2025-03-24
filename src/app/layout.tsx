c29fb4d5f94aa2a890f7eccfeb58bb389b6216af
Robin Pahlman <robin.pahlman@gmail.com>
2025-03-24 13:45:24 -0400

    gtag pt 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
+import { GoogleTagManager } from '@next/third-parties/google'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -35,7 +35,7 @@ export default function RootLayout({
     <html lang="en" data-theme="synthwave">
       <body className={inter.className}>{children}</body>
       <GoogleTagManager gtmId="AW-16871823694" />
-      <GoogleAnalytics gaId="G-7E2HZHZ1Y0" />
+      {/* <GoogleAnalytics gaId="G-7E2HZHZ1Y0" /> */}
     </html>
   )
 }
