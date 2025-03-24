fafab031c8898b970e6b40760e971a5ef21bc19f
Robin Pahlman <robin.pahlman@gmail.com>
2025-03-24 12:24:50 -0400

    hero changes

--- src/app/hero.jsx
+++ src/app/hero.jsx
@@ -1,10 +1,21 @@
 import Image from 'next/image'
+import { useState, useEffect } from 'react'
 
 export default function Hero({ heroImage }) {
   const doClick = () => {
     const el = document.getElementById('calendlySection')
     el.scrollIntoView({ behavior: 'smooth' })
   }
+  const options = ['Band Players', 'Singer-Songwriters', 'Music Enthusiasts']
+  const [currentOption, setCurrentOption] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentOption(prev => (prev + 1) % options.length)
+    }, 3000) // Change every 3 seconds
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div className="hero bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -16,8 +27,8 @@ export default function Hero({ heroImage }) {
         <div className="w-full lg:w-1/2">
           <p className="my-4">Unlock your creative potential with</p>
           <h1 className="text-5xl font-bold">
-            <span className="text-secondary">Guitar Lessons</span> Designed for
-            Band Players
+            <span className="text-secondary">Guitar Lessons</span> Designed for{' '}
+            <span className="text-primary">{options[currentOption]}</span>
           </h1>
           <div className="py-6">
             <p className="text-lg">
